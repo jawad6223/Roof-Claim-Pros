95e188d8c0fd96ec09e99fd7cb4603126c5d7312
Hadeed Idrees <jawad.bdo99@gmail.com>
2025-09-18 16:59:22 +0500

    changes in dashboard link

---
+++ app/dashboard dash/page.tsx
@@ -0,0 +1,5 @@
+import ContractorDashboard from '@/components/dashboard/ContractorDashboard';
+
+export default function DashboardPage() {
+  return <ContractorDashboard />;
+}
--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import ContractorDashboard from '@/components/dashboard dash/ContractorDashboard';
-
-export default function DashboardPage() {
-  return <ContractorDashboard />;
-}
