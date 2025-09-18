0698ce08883c290ee7800c03192eae3766e9b014
Hadeed Idrees <jawad.bdo99@gmail.com>
2025-09-18 16:57:12 +0500

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import ContractorDashboard from '@/components/dashboard/ContractorDashboard';
+import ContractorDashboard from '@/components/dashboard dash/ContractorDashboard';
 
 export default function DashboardPage() {
   return <ContractorDashboard />;
